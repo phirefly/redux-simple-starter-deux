57678ee09ea64f167f495239bd035e6704003859
Raphael Villas <raphael.villas@gmail.com>
2017-10-15 12:21:17 -0500

    Initial set up of reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books'
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  //The purpose of this function is to output the mapping of our state, hence a combination of reducers
+  books: BooksReducer
+
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+// A reducer is a function that returns a piece of application state
+
+export default function() {
+  return [
+    {title: "Good night Moon"},
+    {title: "The Martian"},
+    {title: "Auggie and Me"},
+    {title: "The Circle"}
+  ]
+}
